3ca6684e20381565b04f5cb1a65402a492ea9fb2
Joe <joe@showblender.com>
2026-02-20 09:35:39 -0800

    Initial commit

---
+++ RA11y-iOS/RA11y-iOS/RA11y_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RA11y_iOSApp.swift
+//  RA11y-iOS
+//
+//  Created by Joseph McCraw on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct RA11y_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RA11y-iOS/RA11y-iOSTests/RA11y_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  RA11y_iOSTests.swift
+//  RA11y-iOSTests
+//
+//  Created by Joseph McCraw on 2/19/26.
+//
+
+import Testing
+@testable import RA11y_iOS
+
+struct RA11y_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
